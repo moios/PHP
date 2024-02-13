0af293db4d56b55961611e7a1710add5c9a35459
moios <osmarmoises@gmail.com>
2024-02-13 16:18:55 -0300

    ejercicios iniciales de php

---
+++ operadores.php
@@ -0,0 +1,11 @@
+<?php
+//primeras practicas de php
+//definicion de variables
+$a = 8;
+$b = 3;
+print("<h3>operaciones aritmeticas</h3>");
+echo "<br>a = $a y b = $b";
+echo "<br>Suma:".($a+$b);
+echo "<br>Resta:".($a-$b);
+echo "<br>Mul:".($a*$b);
+echo "<br>Div:".($a/$b);
